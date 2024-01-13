a43b7db2cc4053c3e01dc773a0354f70dfca1e13
junior leoncio <graciano.leoncio@gmail.com>
2024-01-13 14:59:00 -0300

    initial commit

---
+++ CoverFlow/CoverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowApp.swift
+//  CoverFlow
+//
+//  Created by leoncio junior on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
